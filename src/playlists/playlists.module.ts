cc5a52e81c9f5c8834b0a6963ca80224a32ed32d
Sergio Sanz García <sergiosanz2004@gmail.com>
2024-06-05 12:33:36 +0200

    fix 23123131

--- src/playlists/playlists.module.ts
+++ src/playlists/playlists.module.ts
@@ -3,11 +3,13 @@ import { PlaylistsService } from './playlists.service';
 import { PlaylistsController } from './playlists.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Playlist, PlaylistSchema } from './entities/playlist.entity';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
   controllers: [PlaylistsController],
   providers: [PlaylistsService],
   imports: [
+    AuthModule,
     MongooseModule.forFeature([
       {
         name: Playlist.name,
